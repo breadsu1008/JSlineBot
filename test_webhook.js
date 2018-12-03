8a2ca7cda1351cf4f7e5e57d59d7b2ec312da8be
Brad <Brad@jsene.com>
2018-12-03 15:29:29 +0800

    alarm

--- test_webhook.js
+++ test_webhook.js
@@ -20,7 +20,6 @@ line.init({
   // (Optional) for webhook signature validation
   channelSecret: 'e93f969b88ab56e1cd3b7fd4f5629461'
 })
-
 /**
  * response example (https://devdocs.line.me/ja/#webhook):
  * {
@@ -50,6 +49,7 @@ app.get('/gettest/',function(req,res,next){
 
 app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
   // get content from request body
+  
   const promises = req.body.events.map(event => {
     // reply message
     return line.client
@@ -72,6 +72,12 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/test/',function (req,res,next) {
+  line.init({
+  accessToken: 'XkspzccnpZrgal6P74uqgX4m8NiD09wH+HCpgWslhp0QmwWnH7ZhpXxULqQufSBBLpVYMPGAKhnS0cgb+JSLwh+L3jrWhXRid4TRLxId9QCcqRQPbC3Xt3CDRqfHDXxfOGdwlF+mzjPJDD7yfPFzeAdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: 'e93f969b88ab56e1cd3b7fd4f5629461'
+})
+
   console.log('in test')
 
           var msg = req.body.text
@@ -99,6 +105,35 @@ app.listen(port_number,()=>{
   
   console.log('Express server listening on port  in mode')
 });
+
+
+
+app.post('/test2/',function(req,res,next){
+  console.log('in test2');
+  line.init({
+    accessToken:'A89SLD2SDnS8887JMNkvjKCcQJahdohlPJITolq06Ci6GmhEabzKS9ce2Mmd2wpCbU8FvIhkcqlO8VwgPSV2/DjmgqmrKvTUW00UZrT8DFnmCM7nY0tudx2UyzvxywYO+Jf1dkOZOflvF5t/7KKaIwdB04t89/1O/w1cDnyilFU=',
+
+    channelSecret:'6abf01d14c90b819c5950c59b10f243f'
+  });
+ 
+      var msg = req.body.text
+
+          console.log(msg)
+          return line.client.pushMessage({
+            to:'C2ef7b7fc64cedf4d0e2c8be95823f8e6', 
+            messages:[{
+              type: 'text',
+              text: msg
+            }]
+          })
+           .then(()=>{
+        res.json({success:true})
+      })
+           .catch((err)=>{
+            res.json({success:false})
+           });
+ 
+})
 /*
 app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port in mode')
